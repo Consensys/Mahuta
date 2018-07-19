74defbb4ab1fc6387ffc71811620cd696b4fcb99
joshorig <cassidy.joshua@gmail.com>
2018-07-19 17:25:11 +0100

    Just rely on default dev version bump

--- scripts/update-development-branch-version.sh
+++ scripts/update-development-branch-version.sh
@@ -4,12 +4,9 @@ echo Git commit message: $GIT_COMMIT_DESC
 echo Git PR Number: $CIRCLE_PR_NUMBER
 if [[ "$GIT_COMMIT_DESC" != *"maven-release-plugin"* && "$CIRCLE_PR_NUMBER" == "" ]]; then
   #get highest tags across all branches, not just the current branch
-  git pull
-  NEW_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-  echo $NEW_VERSION
   git checkout development
 
-  mvn --batch-mode release:update-versions -DdevelopmentVersion=$NEW_DEV_VERSION
+  mvn --batch-mode release:update-versions
 
   git config --global user.email "info@kauri.io"
   git config --global user.name "circleci"
