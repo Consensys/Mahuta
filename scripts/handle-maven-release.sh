3aa276447974d7642543ba2dd43cc05a5a7a4a3a
joshorig <cassidy.joshua@gmail.com>
2018-07-19 15:29:09 +0100

    Add workflows to circleci

--- scripts/handle-maven-release.sh
+++ scripts/handle-maven-release.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+echo Username $CIRCLE_USERNAME
+if [ "$CIRCLE_USERNAME" != "circleci" ]; then
+  RELEASE_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//g')
 
-RELEASE_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//g')
-
-VNUM1="$(cut -d'.' -f1 <<<"$RELEASE_VERSION")"
-VNUM2="$(cut -d'.' -f2 <<<"$RELEASE_VERSION")"
-VNUM3="$(cut -d'.' -f3 <<<"$RELEASE_VERSION")"
-VNUM3=$((VNUM3+1))
+  VNUM1="$(cut -d'.' -f1 <<<"$RELEASE_VERSION")"
+  VNUM2="$(cut -d'.' -f2 <<<"$RELEASE_VERSION")"
+  VNUM3="$(cut -d'.' -f3 <<<"$RELEASE_VERSION")"
+  VNUM3=$((VNUM3+1))
 
 
-#create new version
-NEW_DEV_VERSION="$VNUM1.$VNUM2.$VNUM3"
+  #create new version
+  NEW_DEV_VERSION="$VNUM1.$VNUM2.$VNUM3"
 
-git config --global user.email "cassidy.joshua@gmail.com"
-git config --global user.name "joshorig"
+  git config --global user.email "info@kauri.io"
+  git config --global user.name "circleci"
 
-echo Running release:prepare with releaseVersion:$RELEASE_VERSION and  DdevelopmentVersion:$NEW_DEV_VERSION-SNAPSHOT
+  echo Running release:prepare with releaseVersion:$RELEASE_VERSION and  DdevelopmentVersion:$NEW_DEV_VERSION-SNAPSHOT
 
-mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$NEW_DEV_VERSION-SNAPSHOT
+  mvn -B release:prepare -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$NEW_DEV_VERSION-SNAPSHOT
 
-mvn -s ./.circleci/.circleci.settings.xml release:perform -Darguments="-Dmaven.javadoc.skip=true" -DskipTests
+  mvn -s ./.circleci/.circleci.settings.xml release:perform -Darguments="-Dmaven.javadoc.skip=true" -DskipTests
+fi
--- scripts/update-development-branch-version.sh
+++ scripts/update-development-branch-version.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+echo Username $CIRCLE_USERNAME
+if [ "$CIRCLE_USERNAME" != "circleci" ]; then
+  #get highest tags across all branches, not just the current branch
+  NEW_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 
-#get highest tags across all branches, not just the current branch
-NEW_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  git checkout testtagging
 
-git checkout testtagging
+  mvn --batch-mode release:update-versions -DdevelopmentVersion=$NEW_DEV_VERSION
 
-mvn --batch-mode release:update-versions -DdevelopmentVersion=$NEW_DEV_VERSION
+  git config --global user.email "info@kauri.io"
+  git config --global user.name "circleci"
 
-git add .
-git commit -m "CircleCI build $CIRCLE_BUILD_NUM updating version after tag"
-git push origin testtagging
+  git add .
+  git commit -m "CircleCI build $CIRCLE_BUILD_NUM updating version after tag"
+  git push origin testtagging
+fi
