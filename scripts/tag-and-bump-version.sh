af74946c2aa88f0f43369964b56142b57a9b0164
joshorig <cassidy.joshua@gmail.com>
2018-07-19 12:05:49 +0100

    Add workflows to circleci

--- scripts/tag-and-bump-version.sh
+++ scripts/tag-and-bump-version.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 IPFS_STORE_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//g')
-
-echo $IPFS_STORE_VERSION
+./scripts/gittag.sh $IPFS_STORE_VERSION
 
 NEW_DEV_VERSION=$(scripts/get-next-dev-ver.sh)
 echo $NEW_DEV_VERSION
+
+git checkout testtagging
+
+mvn --batch-mode release:update-versions -DdevelopmentVersion=$NEW_DEV_VERSION-SNAPSHOT
+
+git add .
+git commit -m "CircleCI build $TRAVIS_BUILD_NUMBER updating version after tag"
+git push origin testtagging
