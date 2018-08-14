eed8b5613a76399fc5f7fabd7bd7683204f6ac24
gjeanmart <gregoire.jeanmart@consensys.net>
2018-08-14 17:22:16 +0100

    remove javascript samples ...

--- ipfs-store-service/src/test/resources/frontend/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(9090, function(){
-    console.log('Server running on 9090...');
-});
