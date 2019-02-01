3c58442f9770eeb928c36980651ab75ff3ccc56d
gjeanmart <gregoire.jeanmart@consensys.net>
2019-02-01 13:59:24 +0000

    docker: auto-release snapshot image

--- .scripts/build-docker-image.sh
+++ .scripts/build-docker-image.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+TYPE=$1
+echo Release type: $TYPE
+
 GIT_COMMIT_DESC=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
 echo Git commit message: $GIT_COMMIT_DESC
 echo Git PR Number: $CIRCLE_PR_NUMBER
-if [[ "$GIT_COMMIT_DESC" != *"maven-release-plugin"* && "$CIRCLE_PR_NUMBER" == "" ]]; then
-  RELEASE_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//g')
-  echo building docker with version: ${RELEASE_VERSION}
-  set -e
 
-  docker login -u ${DOCKER_HUB_USER_ID} -p ${DOCKER_HUB_PWD}
-  docker build -t gjeanmart/mahuta:${RELEASE_VERSION} -f mahuta-service/Dockerfile mahuta-service/.
-  docker build -t gjeanmart/mahuta:latest -f mahuta-service/Dockerfile mahuta-service/.
-  docker push gjeanmart/mahuta:${RELEASE_VERSION}
-  docker push gjeanmart/mahuta:latest
+if [[ "$TYPE" == "latest" ]]; then
+  VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec | sed 's/-SNAPSHOT//g')
+fi
+
+if [[ "$TYPE" == "snapshot" ]]; then
+  VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+fi
+
+if [[ -v VERSION ]]; then
+    docker login -u ${DOCKER_HUB_USER_ID} -p ${DOCKER_HUB_PWD}
+
+    echo building docker with version: ${VERSION}
+    set -e
+
+    docker build -t gjeanmart/mahuta:${VERSION} -f mahuta-service/Dockerfile mahuta-service/.
+    docker build -t gjeanmart/mahuta:${TYPE} -f mahuta-service/Dockerfile mahuta-service/.
+    docker push gjeanmart/mahuta:${VERSION}
+    docker push gjeanmart/mahuta:${TYPE}
 fi
