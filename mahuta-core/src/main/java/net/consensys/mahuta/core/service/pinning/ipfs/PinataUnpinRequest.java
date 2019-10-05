6c0290f54dc89b76ad48c1be4ca25d26e4ad9a91
gjeanmart <gregoire.jeanmart@gmail.com>
2019-10-05 17:46:07 +0100

    Pinata pinning service integrated as replica

---
+++ mahuta-core/src/main/java/net/consensys/mahuta/core/service/pinning/ipfs/PinataUnpinRequest.java
@@ -0,0 +1,19 @@
+package net.consensys.mahuta.core.service.pinning.ipfs;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor @NoArgsConstructor
+@ToString
+public class PinataUnpinRequest {
+
+	@JsonProperty("ipfs_pin_hash")
+	private @Getter String h;
+	
+	
+	
+}
